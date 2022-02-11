69a38ed46bb4830c08f338a560a671a70e54019d
Ralph Soika <ralph.soika@imixs.com>
2022-02-11 18:23:47 +0100

    removed theia module

--- imixs-bpmn.glsp-theia/examples/workspace/example1.md
+++
@@ -1,5 +0,0 @@
-= Documentation =
-
-== Push ==
-
-The push task is performed by the user. He or she pushes the button.
--- imixs-bpmn.glsp-theia/packages/theia-integration/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-    extends: '@eclipse-glsp',
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: 'tsconfig.json'
-    }
-};
