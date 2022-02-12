e90442892e3cfbacc35d99416a3511e40ab183d3
Ralph Soika <ralph.soika@imixs.com>
2022-02-12 12:57:30 +0100

    update

--- glsp-client/minimal-theia/src/node/minimal-glsp-server-contribution.ts
+++ glsp-client/minimal-theia/src/node/minimal-glsp-server-contribution.ts
@@ -23,7 +23,7 @@ export const DEFAULT_PORT = 5007;
 export const PORT_ARG_KEY = 'MINIMAL_GLSP';
 export const LOG_DIR = join(__dirname, '..', '..', 'logs');
 const JAR_FILE = resolve(
-    join(__dirname, '..', '..', '..', '..', 'glsp-server', 'target', 'org.eclipse.glsp.example.minimal-0.9.0-glsp.jar')
+    join(__dirname, '..', '..', '..', '..', 'glsp-server', 'target', 'org.imixs.bpmn.glsp.server-0.0.1-SNAPSHOT-glsp.jar')
 );
 
 @injectable()
