a26bd2b5fffb2ef97f9bcff09eb7687f27200ae6
Ralph Soika <ralph.soika@imixs.com>
2022-02-20 10:52:16 +0100

    started 0.0.5

--- imixs-bpmn.glsp-client/bpmn-theia/src/node/bpmn-glsp-server-contribution.ts
+++ imixs-bpmn.glsp-client/bpmn-theia/src/node/bpmn-glsp-server-contribution.ts
@@ -23,7 +23,7 @@ export const DEFAULT_PORT = 5007;
 export const PORT_ARG_KEY = 'GLSP_PORT';
 export const LOG_DIR = join(__dirname, '..', '..', 'logs');
 const JAR_FILE = resolve(
-    join(__dirname, '..', '..', '..', '..', 'imixs-bpmn.glsp-server', 'target', 'org.imixs.bpmn.glsp.server-0.0.4-SNAPSHOT-glsp.jar')
+    join(__dirname, '..', '..', '..', '..', 'imixs-bpmn.glsp-server', 'target', 'org.imixs.bpmn.glsp.server-0.0.5-SNAPSHOT-glsp.jar')
 );
 
 @injectable()
