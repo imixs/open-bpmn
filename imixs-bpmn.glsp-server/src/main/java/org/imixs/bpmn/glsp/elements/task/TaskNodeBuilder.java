0644c9af04187fe072087de88a68a639635c02ca
Ralph Soika <ralph.soika@imixs.com>
2022-02-26 22:46:19 +0100

    layouting

--- imixs-bpmn.glsp-server/src/main/java/org/imixs/bpmn/glsp/elements/task/TaskNodeBuilder.java
+++ imixs-bpmn.glsp-server/src/main/java/org/imixs/bpmn/glsp/elements/task/TaskNodeBuilder.java
@@ -58,23 +58,30 @@ public class TaskNodeBuilder extends AbstractGNodeBuilder<TaskNode, TaskNodeBuil
       node.getCategory().add(taskType);
 
       node.setLayout(GConstants.Layout.HBOX);
-      node.getLayoutOptions().put("paddingRight", 10);
+      // node.getLayoutOptions().put("paddingRight", 10);
 
       // Set min width/height
       node.getLayoutOptions().put("minWidth", 160);
       node.getLayoutOptions().put("minHeight", 60);
 
-      node.getLayoutOptions().put(H_ALIGN, "center");
-      node.getLayoutOptions().put(H_GRAB, false);
-      node.getLayoutOptions().put(V_GRAB, false);
+      // node.getLayoutOptions().put("paddingTop", 80);
 
+      // node.getLayoutOptions().put(H_ALIGN, "center");
+      // node.getLayoutOptions().put(H_GRAB, false);
+      // node.getLayoutOptions().put(V_GRAB, false);
+
+      node.getLayoutOptions().put("hGap", 10);
+      node.getLayoutOptions().put("vAlign", "center");
       node.getChildren().add(createCompartmentIcon(node));
       node.getChildren().add(createCompartmentHeader(node));
 
    }
 
    private Icon createCompartmentIcon(final TaskNode taskNode) {
-      return new IconBuilder().id(taskNode.getId() + "_icon").build();
+      return new IconBuilder()
+         .id(taskNode.getId() + "_icon")
+         // .position(-10, -10)
+         .build();
    }
 
    private GLabel createCompartmentHeader(final TaskNode taskNode) {
