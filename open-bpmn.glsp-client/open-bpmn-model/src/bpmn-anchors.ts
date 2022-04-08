749c3b753baa9aa414651e8d74c3836882acfd30
Ralph Soika <ralph.soika@imixs.com>
2022-04-08 08:54:00 +0200

    smaller improvements - still draft
    Issue #27

--- open-bpmn.glsp-client/open-bpmn-model/src/bpmn-anchors.ts
+++ open-bpmn.glsp-client/open-bpmn-model/src/bpmn-anchors.ts
@@ -22,9 +22,9 @@ import { injectable } from 'inversify';
 import {
 	Bounds,
 	Point,
-	center,
+	center,includes,almostEquals,
 	ManhattanEdgeRouter,
-	SConnectableElement,
+	SConnectableElement,RectangleAnchor,PolylineEdgeRouter,
 	IAnchorComputer
 } from 'sprotty';
 
@@ -46,6 +46,43 @@ export class BPMNElementAnchor implements IAnchorComputer {
 		return BPMNElementAnchor.KIND;
 	}
 
+    getAnchor(connectable: SConnectableElement, refPoint: Point, offset: number): Point {
+		console.log('...bin in neuer BPMNElementAnchor methode..');
+        const b = connectable.bounds;
+        if (b.width <= 0 || b.height <= 0) {
+            return b;
+        }
+        const bounds: Bounds = {
+            x: b.x - offset,
+            y: b.y - offset,
+            width: b.width + 2 * offset,
+            height: b.height + 2 * offset
+        };
+        if (refPoint.x >= bounds.x && bounds.x + bounds.width >= refPoint.x) {
+            if (refPoint.y < bounds.y + 0.5 * bounds.height)
+            return { x: refPoint.x, y: bounds.y };
+            else
+            return { x: refPoint.x, y: bounds.y + bounds.height };
+        }
+        if (refPoint.y >= bounds.y && bounds.y + bounds.height >= refPoint.y) {
+            if (refPoint.x < bounds.x + 0.5 * bounds.width)
+            return { x: bounds.x, y: refPoint.y };
+            else
+            return { x: bounds.x + bounds.width, y: refPoint.y };
+        }
+        return center(bounds);
+    }
+}
+
+@injectable()
+export class BPMNElementAnchorNew implements IAnchorComputer {
+
+	static KIND = ManhattanEdgeRouter.KIND + ':' + BPMN_ELEMENT_ANCHOR_KIND;
+
+	get kind(): string {
+		return BPMNElementAnchorNew.KIND;
+	}
+
 	getAnchor(connectable: SConnectableElement, refPoint: Point, offset: number): Point {
 		const b = connectable.bounds;
 		if (b.width <= 0 || b.height <= 0) {
@@ -58,7 +95,7 @@ export class BPMNElementAnchor implements IAnchorComputer {
 			width: b.width + 2 * offset,
 			height: b.height + 2 * offset
 		};
-		console.log('...BPMNElementAnchor initial-bounds x=' + bounds.x + ' y=' + bounds.y + ' w=' + bounds.width + ' h=' + bounds.height);
+		console.log('...BPMNElementAnchor refPoint= ' + refPoint.x + ',' + refPoint.y + ' bounds x=' + bounds.x + ' y=' + bounds.y + ' w=' + bounds.width + ' h=' + bounds.height);
 
 		/*
 		 * The refPoint is between west and east
@@ -87,7 +124,7 @@ export class BPMNElementAnchor implements IAnchorComputer {
 			}
 		}
 		// default....
-		console.log('..WARNING - default to center!');
+		console.warn('... -> default to center!');
 		return center(bounds);
 	}
 }
@@ -101,11 +138,32 @@ export class BPMNPolylineElementAnchor implements IAnchorComputer {
         return BPMNPolylineElementAnchor.KIND + ':' + BPMN_ELEMENT_ANCHOR_KIND;
     }
 
+    /**
+     * Compute an anchor position for routing an edge towards this element.
+     *
+     * The default implementation returns the element's center point. If edges should be connected
+     * differently, e.g. to some point on the boundary of the element's view, the according computation
+     * should be implemented in a subclass by overriding this method.
+     *
+     * @param connectable The node or port an edge should be connected to.
+     * @param referencePoint The point from which the edge is routed towards this elemet, in the same
+     *                       coordintae system as the connectable.
+     * @param offset An optional offset value to be considered in the anchor computation;
+     *               positive values should shift the anchor away from this element, negative values
+     *               should shift the anchor more to the inside. Use this to adapt ot arrow heads.
+     */
 	getAnchor(connectable: SConnectableElement, refPoint: Point, offset: number): Point {
 		const b = connectable.bounds;
 		if (b.width <= 0 || b.height <= 0) {
+			console.warn('... bounds width is <=0!!!');
 			return b;
 		}
+
+		if (b.x === undefined || isNaN(b.x)) {
+			return refPoint;
+		}
+
+		console.log(' bound b.x='+b.x);
 
 		const bounds: Bounds = {
 			x: b.x - offset,
@@ -113,7 +171,21 @@ export class BPMNPolylineElementAnchor implements IAnchorComputer {
 			width: b.width + 2 * offset,
 			height: b.height + 2 * offset
 		};
-		console.log('...BPMNPolylineElementAnchor initial-bounds x=' + bounds.x + ' y=' + bounds.y + ' w=' + bounds.width + ' h=' + bounds.height);
+
+		if (isNaN(bounds.x )) {
+			console.warn('... case 1');
+			return refPoint;
+		}
+
+		console.log('...BPMNPolylineElementAnchor elementID=' + connectable.id + ' refPoint= ' + refPoint.x + ',' + refPoint.y + ' bounds x=' + bounds.x + ' y=' + bounds.y + ' w=' + bounds.width + ' h=' + bounds.height);
+
+        /*
+         * Checks whether the refPoint is included in the element bounds?
+         * Than we return the center of the element
+         */
+        if (includes(bounds,refPoint)) {
+			return center(bounds);
+		}
 
 		/*
 		 * The refPoint is between west and east
@@ -142,7 +214,78 @@ export class BPMNPolylineElementAnchor implements IAnchorComputer {
 			}
 		}
 		// default....
-		console.log('..WARNING - default to center!');
+		console.warn('... -> default to center!');
 		return center(bounds);
 	}
 }
+
+@injectable()
+export class BPMNPolylineElementAnchorNew extends  RectangleAnchor {
+
+    get kind(): string {
+        return PolylineEdgeRouter.KIND + ':' + BPMN_ELEMENT_ANCHOR_KIND;
+    }
+
+    getAnchor(connectable: SConnectableElement, refPoint: Point, offset: number): Point {
+		console.log('...bin in neuer PolylineAnchor methode..');
+        const bounds: any= connectable.bounds;
+        const c: any = center(bounds);
+        const finder = new NearestPointFinder(c, refPoint);
+        if (!almostEquals(c.y, refPoint.y)) {
+            const xTop = this.getXIntersection(bounds.y, c, refPoint);
+            if (xTop >= bounds.x && xTop <= bounds.x + bounds.width)
+                finder.addCandidate(xTop, bounds.y - offset);
+            const xBottom = this.getXIntersection(bounds.y + bounds.height, c, refPoint);
+            if (xBottom >= bounds.x && xBottom <= bounds.x + bounds.width)
+                finder.addCandidate(xBottom, bounds.y + bounds.height + offset);
+        }
+        if (!almostEquals(c.x, refPoint.x)) {
+            const yLeft = this.getYIntersection(bounds.x, c, refPoint);
+            if (yLeft >= bounds.y && yLeft <= bounds.y + bounds.height)
+                finder.addCandidate(bounds.x - offset, yLeft);
+            const yRight = this.getYIntersection(bounds.x + bounds.width, c, refPoint);
+            if (yRight >= bounds.y && yRight <= bounds.y + bounds.height)
+                finder.addCandidate(bounds.x + bounds.width + offset, yRight);
+        }
+        return finder.best;
+    }
+
+    protected getXIntersection(yIntersection: number, centerPoint: Point, point: Point): number {
+        const t = (yIntersection - centerPoint.y) / (point.y - centerPoint.y);
+        return (point.x - centerPoint.x) * t + centerPoint.x;
+    }
+
+    protected getYIntersection(xIntersection: number, centerPoint: Point, point: Point): number {
+        const t = (xIntersection - centerPoint.x) / (point.x - centerPoint.x);
+        return (point.y - centerPoint.y) * t + centerPoint.y;
+    }
+}
+
+class NearestPointFinder {
+    protected currentBest: Point | undefined;
+    protected currentDist: number;
+
+    constructor(protected centerPoint: Point, protected refPoint: Point) {
+    }
+
+    addCandidate(x: number, y: number) {
+        const dx = this.refPoint.x - x;
+        const dy = this.refPoint.y - y;
+        const dist = dx * dx + dy * dy;
+        if (this.currentDist < 0 || dist < this.currentDist) {
+            this.currentBest = {
+                x: x,
+                y: y
+            };
+            this.currentDist = dist;
+        }
+    }
+
+    get best(): Point {
+        if (this.currentBest === undefined)
+            return this.centerPoint;
+        else
+            return this.currentBest;
+    }
+}
+
