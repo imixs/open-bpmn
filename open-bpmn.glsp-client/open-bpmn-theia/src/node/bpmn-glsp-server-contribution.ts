9beca368c6d6f5861a420916d7ea9742978fb80f
Ralph Soika <ralph.soika@imixs.com>
2022-04-10 19:10:15 +0200

    some improvements in project setup
    issue #28

--- open-bpmn.glsp-client/open-bpmn-theia/src/node/bpmn-glsp-server-contribution.ts
+++ open-bpmn.glsp-client/open-bpmn-theia/src/node/bpmn-glsp-server-contribution.ts
@@ -13,8 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import {getPort} from '@eclipse-glsp/theia-integration/lib/node';
-import { JavaSocketServerContribution, JavaSocketServerLaunchOptions } from '@eclipse-glsp/theia-integration/lib/node';
+import { getPort, JavaSocketServerContribution, JavaSocketServerLaunchOptions } from '@eclipse-glsp/theia-integration/lib/node';
 import { injectable } from '@theia/core/shared/inversify';
 import { join, resolve } from 'path';
 import { BPMNLanguage } from '../common/bpmn-language';
---
+++ wipe-full.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Wipe out all node.js packages
+#
+echo "***************************************"
+echo "* Wipe Out - full mode.....           *"
+echo "***************************************"
+
+#rm -R ./open-bpmn-app/lib
+#rm -R ./open-bpmn-app/node_modules
+#rm -R ./open-bpmn-app/src-gen
+#rm -R ./open-bpmn-app/gen-webpack*
+#rm -R ./open-bpmn-app/webpack*
+#rm -R ./open-bpmn-app/logs
+
+#rm -R ./open-bpmn-glsp/lib
+#rm -R ./open-bpmn-glsp/node_modules
+
+#rm -R ./open-bpmn-theia/lib
+#rm -R ./open-bpmn-theia/node_modules
+
+#rm -R node_modules
+
+#rm yarn.lock
+# Remove files from gitignore
+
+git clean -xdf
