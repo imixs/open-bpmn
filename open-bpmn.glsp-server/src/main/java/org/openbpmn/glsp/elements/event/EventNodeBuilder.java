d3c7ebc4cb8ad565856667c14816fb5f5bac3813
Ralph Soika <ralph.soika@imixs.com>
2022-06-06 17:33:19 +0200

    impl
    Issue #63

--- open-bpmn.glsp-server/src/main/java/org/openbpmn/glsp/elements/event/EventNodeBuilder.java
+++ open-bpmn.glsp-server/src/main/java/org/openbpmn/glsp/elements/event/EventNodeBuilder.java
@@ -1,28 +1,27 @@
 /********************************************************************************
- * Copyright (c) 2020 EclipseSource and others.
+ * Copyright (c) 2022 Imixs Software Solutions GmbH,
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is available at
- * https://www.gnu.org/software/classpath/license.html.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * You can receive a copy of the GNU General Public
+ * License at http://www.gnu.org/licenses/gpl.html
+ *
+ * Project:
+ *     https://github.com/imixs/open-bpmn
+ *
+ * Contributors:
+ *     Imixs Software Solutions GmbH - Project Management
+ *     Ralph Soika - Software Developer
  ********************************************************************************/
 package org.openbpmn.glsp.elements.event;
-
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.json.Json;
-import javax.json.JsonObject;
 
 import org.eclipse.glsp.graph.builder.AbstractGNodeBuilder;
 import org.eclipse.glsp.graph.util.GConstants;
@@ -30,9 +29,6 @@ import org.eclipse.glsp.graph.util.GraphUtil;
 import org.openbpmn.bpmn.elements.BPMNEvent;
 import org.openbpmn.glsp.bpmn.BpmnFactory;
 import org.openbpmn.glsp.bpmn.EventNode;
-import org.openbpmn.glsp.jsonforms.SchemaBuilder;
-import org.openbpmn.glsp.jsonforms.UISchemaBuilder;
-import org.openbpmn.glsp.jsonforms.UISchemaBuilder.Layout;
 import org.openbpmn.glsp.utils.BPMNBuilderHelper;
 
 /**
@@ -71,10 +67,6 @@ public class EventNodeBuilder extends AbstractGNodeBuilder<EventNode, EventNodeB
     public void setProperties(final EventNode node) {
         super.setProperties(node);
         node.setName(name);
-        node.getArgs().put("JSONFormsData", buildJSONFormsData());
-        // node.getArgs().put("JSONFormsUISchema", buildJSONFormsUISchemaSimple());
-        node.getArgs().put("JSONFormsUISchema", buildJSONFormsUISchemaTabs());
-        node.getArgs().put("JSONFormsSchema", buildSchema());
 
         node.setLayout(GConstants.Layout.FREEFORM);
         size = GraphUtil.dimension(BPMNEvent.DEFAULT_WIDTH, BPMNEvent.DEFAULT_HEIGHT);
@@ -84,91 +76,6 @@ public class EventNodeBuilder extends AbstractGNodeBuilder<EventNode, EventNodeB
         node.getLayoutOptions().put("minHeight", BPMNEvent.DEFAULT_HEIGHT);
 
         node.getChildren().add(BPMNBuilderHelper.createCompartmentIcon(node));
-
-    }
-
-    /**
-     * This Helper Method generates a JSON Object with the BPMNElement properties.
-     * <p>
-     * This json object is used on the GLSP Client to generate the EMF JsonForms
-     *
-     * @return
-     */
-    private String buildJSONFormsData() {
-        String result = "{}";
-        JsonObject jsonObject = Json.createObjectBuilder() //
-                .add("name", name) //
-                .add("category", "some cati") //
-                .add("documentation", "some test docu") //
-                .build();
-
-        try (Writer writer = new StringWriter()) {
-            Json.createWriter(writer).write(jsonObject);
-            result = writer.toString();
-        } catch (IOException e) {
-            result = "{}";
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    /**
-     * This Helper Method generates a UISchema JSON Object used on the GLSP Client
-     * to generate the EMF JsonForms
-     *
-     * @return
-     */
-    private String buildJSONFormsUISchemaSimple() {
-
-        return new UISchemaBuilder(Layout.VERTICAL). //
-                addElements("name", "category", "documentation"). //
-                build();
-
-    }
-
-    /**
-     * Returns the Data Schema for a event
-     *
-     * @return
-     */
-    private String buildSchema() {
-
-        return new SchemaBuilder(). //
-                addProperty("name", "string", "Please enter your name :-)"). //
-                addProperty("category", "string", null). //
-                addProperty("documentation", "string", null). //
-                build();
-
-    }
-
-    /**
-     * This Helper Method generates a UISchema JSON Object used on the GLSP Client
-     * to generate the EMF JsonForms
-     * <p>
-     * The UI schema, which is passed to JSON Forms, describes the general layout of
-     * a form and is just a regular JSON object. It describes the form by means of
-     * different UI schema elements, which can often be categorized into either
-     * Controls or Layouts.
-     *
-     * @return
-     */
-    private String buildJSONFormsUISchemaTabs() {
-        String result = "{}";
-
-        Map<String, String> multilineOption = new HashMap<>();
-        multilineOption.put("multi", "true");
-        result = new UISchemaBuilder(Layout.CATEGORIZATION). //
-                addCategory("General"). //
-                addLayout(Layout.HORIZONTAL). //
-                addElements("name", "category"). //
-                addCategory("Attributes"). //
-                addLayout(Layout.VERTICAL). //
-                addElement("documentation", "Documentation", multilineOption). //
-                addCategory("Workflow"). //
-                addLayout(Layout.HORIZONTAL). //
-                build();
-
-        return result;
 
     }
 
