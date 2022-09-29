69df2369f0ec50f65169b41c90a8e1935fe870cb
Ralph Soika <ralph.soika@imixs.com>
2022-09-29 21:39:28 +0200

    impl
    Issue #80

--- changeVersion.sh
+++ changeVersion.sh
@@ -39,8 +39,8 @@ echo "Replacing now..."
 # replace regular maven versions
 find * -name 'pom.xml' | xargs perl -pi -e "s/<version>$SOURCE_VERSION_PATTERN_MAVEN<\/version>/<version>${TARGET_VERSION}-SNAPSHOT<\/version>/g"
 
-# replace manifest versions
-# Example:
+# replace manifest and nodejs versions
+# Examples:
 # "version": "0.3.0",
 # "@open-bpmn/open-bpmn-model": "0.3.0",
 find * -name 'package.json' | xargs perl -pi -e "s/\"version\": \"${SOURCE_VERSION}\",/\"version\": \"$TARGET_VERSION\",/g"
@@ -48,9 +48,9 @@ find * -name 'package.json' | xargs perl -pi -e "s/\"\@open-bpmn\/open-bpmn-mode
 find * -name 'package.json' | xargs perl -pi -e "s/\"\@open-bpmn\/open-bpmn-glsp\": \"${SOURCE_VERSION}\"/\"\@open-bpmn\/open-bpmn-glsp\": \"$TARGET_VERSION\"/g"
 find * -name 'package.json' | xargs perl -pi -e "s/\"\@open-bpmn\/open-bpmn-properties\": \"${SOURCE_VERSION}\"/\"\@open-bpmn\/open-bpmn-properties\": \"$TARGET_VERSION\"/g"
 find * -name 'package.json' | xargs perl -pi -e "s/\"\@open-bpmn\/open-bpmn-theia\": \"${SOURCE_VERSION}\"/\"\@open-bpmn\/open-bpmn-theia\": \"$TARGET_VERSION\"/g"
-
-
-
+find * -name 'lerna.json' | xargs perl -pi -e "s/\"version\": \"${SOURCE_VERSION}\",/\"version\": \"$TARGET_VERSION\",/g"
+find * -name 'launch.json' | xargs perl -pi -e "s/\"version\": \"${SOURCE_VERSION}\",/\"version\": \"$TARGET_VERSION\",/g"
+find * -name 'bpmn-glsp-server-contribution.ts' | xargs perl -pi -e "s/open-bpmn.server-${SOURCE_VERSION}/open-bpmn.server-$TARGET_VERSION/g"
 
 
 echo "DONE!"
