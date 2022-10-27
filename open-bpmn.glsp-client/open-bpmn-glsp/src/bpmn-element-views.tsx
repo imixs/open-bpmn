fb69771144abef9a6bd1e0be1448ffc0aeb71db2
Ralph Soika <ralph.soika@imixs.com>
2022-10-27 12:50:41 +0200

    fixed
    Issue #105

--- open-bpmn.glsp-client/open-bpmn-glsp/src/bpmn-element-views.tsx
+++ open-bpmn.glsp-client/open-bpmn-glsp/src/bpmn-element-views.tsx
@@ -46,7 +46,8 @@ const JSX = { createElement: svg };
 export class IconView extends ShapeView {
     render(element: Icon, context: RenderingContext): VNode | undefined {
         let scaleFactor = 1;
-        let translate = 0;
+        let translateX = 0;
+        let translateY = 0;
         if (!this.isVisible(element, context)) {
             return undefined;
         }
@@ -83,12 +84,16 @@ export class IconView extends ShapeView {
                 icon =
                     // eslint-disable-next-line max-len
                     'M6 10V9h9v1H6zm4-4h5v1h-5V6zm5-3v1H6V3h9zm-9 9v1h9v-1H6z"/><path fill-rule="evenodd" clip-rule="evenodd" d="M1 2.795l.783-.419 5.371 3.581v.838l-5.371 3.581L1 9.957V2.795zm1.007.94v5.281l3.96-2.64-3.96-2.64z';
-            }
+            } else {
+                // dummy place holder
+                icon = 'M6';
+            }            
         }
 
         if (gatewayNode) {
             scaleFactor = 1.5;
-            translate = 9.0;
+            translateX = 9.0;
+            translateY = 9.0;
             if (gatewayNode.type === 'parallelGateway') {
                 // From codicons: https://github.com/microsoft/vscode-codicons/blob/main/src/icons/add.svg
                 icon =
@@ -110,18 +115,18 @@ export class IconView extends ShapeView {
 
         if (eventNode) {
             scaleFactor = 1.5;
-            translate = 4.0;
-
+            translateX = 4.0;
+            translateY = 4.0;
             if (eventNode.symbol === 'messageEventDefinition') {
                 // From codicons: https://github.com/microsoft/vscode-codicons/blob/main/src/icons/mail.svg?short_path=d02764e
                 icon =
                     // eslint-disable-next-line max-len
                     'M1 3.5l.5-.5h13l.5.5v9l-.5.5h-13l-.5-.5v-9zm1 1.035V12h12V4.536L8.31 8.9H7.7L2 4.535zM13.03 4H2.97L8 7.869 13.03 4z';
             } else if (eventNode.symbol === 'conditionalEventDefinition') {
-                // From codicons: https://github.com/microsoft/vscode-codicons/blob/main/src/icons/debug-line-by-line.svg?short_path=a0335ca
+	            // https://github.com/microsoft/vscode-codicons/blob/main/src/icons/checklist.svg?short_path=e850b9d
                 icon =
                     // eslint-disable-next-line max-len
-                    'M1 2.795l.783-.419 5.371 3.581v.838l-5.371 3.581L1 9.957V2.795zm1.007.94v5.281l3.96-2.64-3.96-2.64z';
+                    'M3.75 4.48h-.71L2 3.43l.71-.7.69.68L4.81 2l.71.71-1.77 1.77zM6.99 3h8v1h-8V3zm0 3h8v1h-8V6zm8 3h-8v1h8V9zm-8 3h8v1h-8v-1zM3.04 7.48h.71l1.77-1.77-.71-.7L3.4 6.42l-.69-.69-.71.71 1.04 1.04zm.71 3.01h-.71L2 9.45l.71-.71.69.69 1.41-1.42.71.71-1.77 1.77zm-.71 3.01h.71l1.77-1.77-.71-.71-1.41 1.42-.69-.69-.71.7 1.04 1.05z';
             } else if (eventNode.symbol === 'compensationEventDefinition') {
                 // From codicons:
                 // https://github.com/microsoft/vscode-codicons/blob/main/src/icons/debug-reverse-continue.svg?short_path=5509580
@@ -143,11 +148,20 @@ export class IconView extends ShapeView {
                 icon =
                     // eslint-disable-next-line max-len
                     'M2.78 2L2 2.41v12l.78.42 9-6V8l-9-6zM3 13.48V3.35l7.6 5.07L3 13.48z';
+                translateX=6;
             } else if (eventNode.symbol === 'errorEventDefinition') {
                 // From codicons: https://github.com/microsoft/vscode-codicons/blob/main/src/icons/github-action.svg?short_path=987d495
                 icon =
                     // eslint-disable-next-line max-len
                     'M3.04 10h2.58l.65 1H2.54l-.5-.5v-9l.5-.5h12l.5.5v4.77l-1-1.75V2h-11v8zm5.54 1l-1.41 3.47h2.2L15 8.7 14.27 7h-1.63l.82-1.46L12.63 4H9.76l-.92.59-2.28 5L7.47 11h1.11zm1.18-6h2.87l-1.87 3h3.51l-5.76 5.84L10.2 10H7.47l2.29-5zM6.95 7H4.04V6H7.4l-.45 1zm-.9 2H4.04V8H6.5l-.45 1z';
+            } else if (eventNode.symbol === 'terminateEventDefinition') {
+                // From codicons: https://github.com/microsoft/vscode-codicons/blob/main/src/icons/circle-large.svg?short_path=f801c32
+                icon =
+                    // eslint-disable-next-line max-len
+                    'M9.588 2.215A5.808 5.808 0 0 0 8 2c-.554 0-1.082.073-1.588.215l-.006.002c-.514.141-.99.342-1.432.601A6.156 6.156 0 0 0 2.82 4.98l-.002.004A5.967 5.967 0 0 0 2.21 6.41 5.986 5.986 0 0 0 2 8c0 .555.07 1.085.21 1.591a6.05 6.05 0 0 0 1.548 2.651c.37.365.774.677 1.216.94a6.1 6.1 0 0 0 1.435.609A6.02 6.02 0 0 0 8 14c.555 0 1.085-.07 1.591-.21.515-.145.99-.348 1.426-.607l.004-.002a6.16 6.16 0 0 0 2.161-2.155 5.85 5.85 0 0 0 .6-1.432l.003-.006A5.807 5.807 0 0 0 14 8c0-.554-.072-1.082-.215-1.588l-.002-.006a5.772 5.772 0 0 0-.6-1.423l-.002-.004a5.9 5.9 0 0 0-.942-1.21l-.008-.008a5.902 5.902 0 0 0-1.21-.942l-.004-.002a5.772 5.772 0 0 0-1.423-.6l-.006-.002zm4.455 9.32a7.157 7.157 0 0 1-2.516 2.508 6.966 6.966 0 0 1-1.668.71A6.984 6.984 0 0 1 8 15a6.984 6.984 0 0 1-1.86-.246 7.098 7.098 0 0 1-1.674-.711 7.3 7.3 0 0 1-1.415-1.094 7.295 7.295 0 0 1-1.094-1.415 7.098 7.098 0 0 1-.71-1.675A6.985 6.985 0 0 1 1 8c0-.643.082-1.262.246-1.86a6.968 6.968 0 0 1 .711-1.667 7.156 7.156 0 0 1 2.509-2.516 6.895 6.895 0 0 1 1.675-.704A6.808 6.808 0 0 1 8 1a6.8 6.8 0 0 1 1.86.253 6.899 6.899 0 0 1 3.083 1.805 6.903 6.903 0 0 1 1.804 3.083C14.916 6.738 15 7.357 15 8s-.084 1.262-.253 1.86a6.9 6.9 0 0 1-.704 1.674z';
+                translateX=2;
+                translateY=2;
+                scaleFactor=1.8;
             } else if (eventNode.symbol === 'multipleEventDefinition') {
                 // From codicons: https://github.com/microsoft/vscode-codicons/blob/main/src/icons/layers.svg?short_path=f67ac65
                 icon =
@@ -157,15 +171,16 @@ export class IconView extends ShapeView {
         }
 
         // did we have now a icon?
+        let vnode: any;
         if (!icon) {
-            return undefined;
+	        return undefined;
+        } else {
+            vnode = (
+              <g>
+                  <path transform={'scale(' + scaleFactor + '),translate(' + translateX + ',' + translateY + ')'} d={icon} />
+              </g>
+          );
         }
-
-        const vnode: any = (
-            <g>
-                <path transform={'scale(' + scaleFactor + '),translate(' + translate + ',' + translate + ')'} d={icon} />
-            </g>
-        );
 
         const subType = getSubType(element);
         if (subType) {
