8cddd10b81a84b6add1a3463ec66a1a7f1e2f9cc
Ralph Soika <ralph.soika@imixs.com>
2023-05-18 17:33:01 +0200

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,7 +16,7 @@ SOURCE_VERSION=$(grep -oP -m1 '(?<=<version>)[^-SNAPSHOT]+' pom.xml)
 echo "...current version: $SOURCE_VERSION"
 
 echo "...release server part.....    "
-mvn clean install
+#mvn clean install
 mvn release:clean release:prepare -DautoVersionSubmodules=true
 mvn release:perform
 
