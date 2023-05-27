f5d5134815919be468ede5351302970977da8061
Ralph Soika <ralph.soika@imixs.com>
2023-05-27 08:45:11 +0200

    improvements
    Issue #261

--- open-bpmn.glsp-client/open-bpmn-glsp/src/bpmn-helperlines.tsx
+++ open-bpmn.glsp-client/open-bpmn-glsp/src/bpmn-helperlines.tsx
@@ -70,6 +70,9 @@ export interface HelperLine {
 @injectable()
 export class BPMNElementSnapper implements ISnapper {
 
+	get outOfRange(): number {
+		return -999999;
+	}
 	// default gird size
 	constructor(public grid: { x: number; y: number } = { x: 10, y: 10 }) {}
 
@@ -96,11 +99,11 @@ export class BPMNElementSnapper implements ISnapper {
 			// if a snapPoint was found and this snapPoint is still in the snapRange,
 			// then we adjust the current mouse Postion. Otherwise we return the current position
 			let snapX = Math.round(position.x / this.grid.x) * this.grid.x;
-			if (snapPoint.x > -1 ) {
+			if (snapPoint.x > this.outOfRange ) {
 				snapX= snapPoint.x ;
 			}
 			let snapY= Math.round(position.y / this.grid.y) * this.grid.y;
-			if ( snapPoint.y > -1  ) {
+			if ( snapPoint.y > this.outOfRange  ) {
 				snapY= snapPoint.y ;
 			}
 			snapPoint = { x: snapX, y: snapY };
@@ -251,53 +254,55 @@ export class BPMNElementSnapper implements ISnapper {
 	 * @param point
 	 * @returns
 	 */
-	private findSnapPointForRoutingPoint(modelElement: SRoutingHandle,point: Point): Point {
-		let x = -1;
-		let y = -1;
+	private findSnapPointForRoutingPoint(modelElement: SRoutingHandle,point: Point): Point  {
+		let x = this.outOfRange;
+		let y = this.outOfRange;
 		let childs: any;
-			const p1=modelElement.parent;
+		const p1=modelElement.parent;
 
-			// we need to find out if we are in a container to get the child elements and the routing point coordinates.
-			let pointIndex=modelElement.pointIndex;
-			if (pointIndex===-1) {
-				pointIndex=0;
+		// we need to find out if we are in a container to get the child elements and the routing point coordinates.
+		const pointIndex=modelElement.pointIndex;
+		if (pointIndex===-1) {
+			// pointIndex=0;
+			return { x: x, y: y };
+		}
+
+		if (p1 instanceof SEdge) {
+			const basisPoint=p1.routingPoints[pointIndex];
+			if (!basisPoint) {
+				// problem
+				return { x: x, y: y };
+				// return undefined;
 			}
+			childs=p1.parent.children;
 
-			if (p1 instanceof SEdge) {
-				const basisPoint=p1.routingPoints[pointIndex];
-				if (!basisPoint) {
-					// problem
-					return { x: x, y: y };
-				}
-				childs=p1.parent.children;
+			const modelElementCenter ={x:basisPoint.x+point.x , y:basisPoint.y+point.y } ;
+			// In the following we iterate over all model elements
+			// and compare the x and y axis of the center points
+			for (const element of childs) {
+				if (isBPMNNode(element) && isBoundsAware(element)) {
 
-				const modelElementCenter ={x:basisPoint.x+point.x , y:basisPoint.y+point.y } ;
-				// In the following we iterate over all model elements
-				// and compare the x and y axis of the center points
-				for (const element of childs) {
-					if (isBPMNNode(element) && isBoundsAware(element)) {
-
-						const elementCenter = Bounds.center(element.bounds);
-						// console.log(' ... found element ' + element.id + ' pos=' + elementCenter.x + ','+elementCenter.y);
-						if (elementCenter && modelElementCenter) {
-							// test horizontal alignment...
-							if (y === -1 && this.isNear(elementCenter.y, modelElementCenter.y)) {
-								// fount horizontal snap point
-								y = elementCenter.y -basisPoint.y;
-							}
-							// test vertical alignment...
-							if (x === -1 && this.isNear(elementCenter.x, modelElementCenter.x)) {
-								// found vertical snap point!
-								x = elementCenter.x-basisPoint.x ;
-							}
+					const elementCenter = Bounds.center(element.bounds);
+					// console.log(' ... found element ' + element.id + ' pos=' + elementCenter.x + ','+elementCenter.y);
+					if (elementCenter) {
+						// test horizontal alignment...
+						if (y === this.outOfRange && this.isNear(elementCenter.y, modelElementCenter.y)) {
+							// fount horizontal snap point
+							y = elementCenter.y -basisPoint.y;
+						}
+						// test vertical alignment...
+						if (x === this.outOfRange && this.isNear(elementCenter.x, modelElementCenter.x)) {
+							// found vertical snap point!
+							x = elementCenter.x-basisPoint.x ;
 						}
 					}
-					if (x > -1 && y > -1) {
-						// we can break here as we found already the maximum of two possible matches.
-						break;
-					}
+				}
+				if (x !== this.outOfRange || y !== this.outOfRange) {
+					// we can break here as we found already the maximum of two possible matches.
+					break;
 				}
 			}
+		}
 
 		// return snapoint (-1,-1 if not match was found)
 		return { x: x, y: y };
