cf22435a6ffa0d81ce32d4bfbfc2ef7e1900d8a9
Ralph Soika <ralph.soika@imixs.com>
2024-02-09 09:47:10 +0100

    impl
    Issue #320

--- open-bpmn.glsp-server/src/main/java/org/openbpmn/glsp/operations/BPMNAutoAlignAction.java
+++ open-bpmn.glsp-server/src/main/java/org/openbpmn/glsp/operations/BPMNAutoAlignAction.java
@@ -1,5 +1,7 @@
 package org.openbpmn.glsp.operations;
 
+import java.util.List;
+
 import org.eclipse.glsp.server.actions.Action;
 
 /**
@@ -10,9 +12,23 @@ public class BPMNAutoAlignAction extends Action {
 
     public static final String KIND = "autoAlign";
 
+    private List<String> elementIds;
+
     public BPMNAutoAlignAction() {
         super(KIND);
+    }
 
+    public BPMNAutoAlignAction(final List<String> elementIds) {
+        super(KIND);
+        this.elementIds = elementIds;
+    }
+
+    public List<String> getElementIds() {
+        return elementIds;
+    }
+
+    public void setElementIds(final List<String> elementIds) {
+        this.elementIds = elementIds;
     }
 
 }
