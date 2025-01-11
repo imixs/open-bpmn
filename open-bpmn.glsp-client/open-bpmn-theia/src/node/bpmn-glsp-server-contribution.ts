e93beefbb7caa0f8d0a48fb4b77ffdc554c1937d
Ralph Soika <ralph.soika@imixs.com>
2025-01-11 15:09:31 +0100

    started client 1.2.4

--- open-bpmn.glsp-client/open-bpmn-theia/src/node/bpmn-glsp-server-contribution.ts
+++ open-bpmn.glsp-client/open-bpmn-theia/src/node/bpmn-glsp-server-contribution.ts
@@ -26,7 +26,7 @@ export const DEFAULT_PORT = 0;
 export const PORT_ARG_KEY = 'GLSP_PORT';
 export const LOG_DIR = join(__dirname, '..', '..', 'logs');
 const JAR_FILE = resolve(
-    join(__dirname, '..', '..', '..', '..', 'open-bpmn.glsp-server', 'target', 'open-bpmn.server-1.2.3-SNAPSHOT-glsp.jar')
+    join(__dirname, '..', '..', '..', '..', 'open-bpmn.glsp-server', 'target', 'open-bpmn.server-1.2.4-SNAPSHOT-glsp.jar')
 );
 
 @injectable()
