3549d95e5ba2bc8c67165e5769f9a2e21bcf7f7b
Ralph Soika <ralph.soika@imixs.com>
2025-07-21 08:46:43 +0200

    fix

--- scripts/launch-docker-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "***************************************"
-echo "* Starting - Open BPMN.....           *"
-echo "***************************************"
-cd /home/node/app/open-bpmn.glsp-client/
-yarn start --hostname=0.0.0.0 --root-dir=/home/node/app/open-bpmn.glsp-client/workspace
-
-
